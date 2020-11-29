056ccff7b8088bec4d4ace909e12067471d02082
Javier Franco <javier.franco18@gmail.com>
2020-11-29 12:19:30 -0500

    Setup face recognition API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Particles from 'react-particles-js';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '8887b3bed77c4d7687d35a45696a5d49'
+ });
 
 const particlesOptions = {
   particles: {
@@ -17,19 +25,64 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Particles className="particles"
-        params={particlesOptions} />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    console.log(clarifaiFace)
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+displayFaceBox = (box) => {
+  this.setState({box: box})
+}
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => {
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Navigation />
+        <Particles className="particles"
+          params={particlesOptions} />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition 
+        imageUrl={this.state.imageUrl}
+        box={this.state.box}/>
+      </div>
+    );
+  }
 }
 
 export default App;
